37bf7ab81039affd720a52323ebebbbd32493b8b
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-06-07 15:00:48 +0200

    init react, eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('test');
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
