25643f01eb076b3e8b0ffde9e88a557fe5f6d9c0
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-06-07 19:35:44 +0200

    Updates

---
+++ precache-manifest.962ce731571655fc2466d1f55400997f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47d737185c0f0d28570fc31bad91bec",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "1d3138acdf0790747392",
+    "url": "/slider/static/js/2.c7d27cc7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/slider/static/js/2.c7d27cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d740dc0596bbc7df2a",
+    "url": "/slider/static/js/main.2235efa5.chunk.js"
+  },
+  {
+    "revision": "357f20790d65904ee460",
+    "url": "/slider/static/js/runtime-main.e305ba24.js"
+  }
+]);
